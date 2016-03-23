6afb0191e234125cd326fceadcba6c8f8356c578
Dustin Walker <duwalker88@gmail.com>
2016-03-22 23:59:43 -0400

    Program mostly works. The calculation is not quite correct but will be improved soon.

--- js/slowTypist.js
+++ js/slowTypist.js
@@ -5,7 +5,6 @@ var app = angular.module('IPDistance', []);
 app.controller('IPDistanceController',
   function IPDistanceController($scope) {
 
-
   $scope.distance = function(entry, newForm){
     if (newForm.$invalid && newForm.$pristine) {
       return "0 cm";
@@ -13,8 +12,107 @@ app.controller('IPDistanceController',
       return "invalid IP address";
     } else { // newForm.$valid
       // Do the actual calculation here
-      return entry.address;
+      return calculateDistance(entry.address);
     };
+  };
+
+  var calculateDistance = function(word) {
+    var arr = [...word];
+    var differenceList = [];
+    for(var len = arr.length, i = 1; i < len; i++){
+      // Calculate distance between current point and previous point
+      // until the end is reached
+      var val0 = parseInt(arr[i-1]);
+      var x0 = 0;
+      var y0 = 0;
+      var val1 = parseInt(arr[i]);
+      var x1 = 0;
+      var y1 = 0;
+
+      switch (val0) {
+        case 0:
+          x0 = 1; y0 = 0;
+          break;
+        case 1:
+          x0 = 0; y0 = 1;
+          break;
+        case 2:
+          x0 = 1; y0 = 1;
+          break;
+        case 3:
+          x0 = 2; y0 = 1;
+          break;
+        case 4:
+          x0 = 0; y0 = 2;
+          break;
+        case 5:
+          x0 = 1; y0 = 2;
+          break;
+        case 6:
+          x0 = 2; y0 = 2;
+          break;
+        case 7:
+          x0 = 0; y0 = 3;
+          break;
+        case 8:
+          x0 = 1; y0 = 3;
+          break;
+        case 9:
+          x0 = 2; y0 = 3;
+          break;
+        default:
+          val0 = ".";
+          x0 = 0; y0 = 0;
+      };
+
+      switch (val1) {
+        case 0:
+          x1 = 1; y1 = 0;
+          break;
+        case 1:
+          x1 = 0; y1 = 1;
+          break;
+        case 2:
+          x1 = 1; y1 = 1;
+          break;
+        case 3:
+          x1 = 2; y1 = 1;
+          break;
+        case 4:
+          x1 = 0; y1 = 2;
+          break;
+        case 5:
+          x1 = 1; y1 = 2;
+          break;
+        case 6:
+          x1 = 2; y1 = 2;
+          break;
+        case 7:
+          x1 = 0; y1 = 3;
+          break;
+        case 8:
+          x1 = 1; y1 = 3;
+          break;
+        case 9:
+          x1 = 2; y1 = 3;
+          break;
+        default:
+          val1 = ".";
+          x1 = 0; y1 = 0;
+      };
+
+      var dxSQ = Math.pow(Math.abs(x0-x1), 2);
+      var dySQ = Math.pow(Math.abs(y0-y1), 2);
+
+      var diff = Math.sqrt( dxSQ + dySQ );
+      console.log(diff + " - dxSQ: " + dxSQ + " - dySQ: " + dySQ );
+      differenceList.push(diff);
+    };
+    var totalDistance = differenceList.reduce(function(a, b) {
+      return a + b;
+    });
+
+    return totalDistance;
   };
 
 });
